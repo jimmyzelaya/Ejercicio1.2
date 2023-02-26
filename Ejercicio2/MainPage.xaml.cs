ef92049961655e31655d1e59af1c9c3d00772a59
Jimmy Zelaya <98270491+jimmyzelaya@users.noreply.github.com>
2023-02-26 14:38:31 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using static SQLite.SQLite3;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // FUNCION ENVIAR
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var datos = new Modelos.Datos
+                {
+                    rNombre = nombre.Text,
+                    rApellidos = apellidos.Text,
+                    rEdad = edad.Text,
+                    rCorreo = correo.Text
+                };
+
+                //await DisplayAlert("Datos", "Nombre " + nombre.Text, "LISTO");
+
+                var pagina = new Paginas.Page2();
+                pagina.BindingContext = datos;
+                await Navigation.PushAsync(pagina);
+
+                ClearControls();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        // LIMPIAR
+        private void ClearControls()
+        {
+            nombre.Text = "";
+            apellidos.Text = "";
+            edad.Text = "";
+            correo.Text = "";
+
+        }
+
+    }
+}
---
+++ Ejercicio2/Modelos/Datos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2.Modelos
+{
+    internal class Datos
+    {
+        public string rNombre { get; set; }
+        public string rApellidos { get; set; }
+        public string rEdad { get; set; }
+        public string rCorreo { get; set; }
+
+    }
+}
